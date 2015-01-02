9a42aeee17a338f1d853da638337e0fd73284356
Marc Tremblay <mtremblay@diablo-technologies.com>
2015-01-02 15:11:18 -0500

    teradimm-1.5.2.70

--- common/util/td_cache.h
+++ common/util/td_cache.h
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                       *
- *    Copyright (c) 2013 Diablo Technologies Inc. ("Diablo").  All       *
+ *    Copyright (c) 2013 Diablo Technologies Inc. ("DIABLO").  All       *
  *    rights reserved.                                                   *
  *                                                                       *
  *    This software is being licensed under a dual license, at Diablo's  *
@@ -15,35 +15,38 @@
  *    any later version located at <http://www.gnu.org/licenses/>.  See  *
  *    the GNU General Public License for more details.                   *
  *                                                                       *
- *    Modified BSD License                                               *
+ *    BSD License                                                        *
  *                                                                       *
- *    If you have explicit permission from Diablo, then (1) use in       *
- *    source and binary forms, with or without modification; as well as  *
- *    (2) redistribution ONLY in binary form, with or without            *
- *    modification; are permitted provided that the following conditions *
- *    are met:                                                           *
+ *    If you have explicit permission from Diablo, then redistribution   *
+ *    and use in source and binary forms, with or without modification,  *
+ *    are permitted provided that the following conditions are met:      *
  *                                                                       *
- *    * Redistributions in binary form must reproduce the above          *
- *    copyright notice, this list of conditions and the following        *
- *    disclaimer in the documentation and/or other materials provided    *
- *    with the distribution.                                             *
+ *        * Redistributions of source code must retain the above         *
+ *        copyright notice, this list of conditions and the following    *
+ *        disclaimer.                                                    *
  *                                                                       *
- *    * Neither the name of the DIABLO nor the names of its contributors *
- *    may be used to endorse or promote products derived from this       *
- *    software without specific prior written permission.                *
+ *        * Redistributions in binary form must reproduce the above      *
+ *        copyright notice, this list of conditions and the following    *
+ *        disclaimer in the documentation and/or other materials         *
+ *        provided with the distribution.                                *
+ *                                                                       *
+ *        * Neither the name of the DIABLO nor the names of its          *
+ *        contributors may be used to endorse or promote products        *
+ *        derived from this software without specific prior written      *
+ *        permission.                                                    *
  *                                                                       *
  *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND             *
  *    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,        *
  *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF           *
  *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           *
  *    DISCLAIMED. IN NO EVENT SHALL DIABLO BE LIABLE FOR ANY DIRECT,     *
- *    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES *
- *    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR *
- *    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) *
- *    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN          *
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR       *
- *    OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,     *
- *    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                 *
+ *    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL         *
+ *    DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   *
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS      *
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,       *
+ *    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING           *
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS *
+ *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.       *
  *                                                                       *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #ifndef __td_cache_h__
